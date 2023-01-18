146a67edc07a1c99c25cc0238355712858699612
Igor Ricci Constantino <igorconstantino701@gmail.com>
2023-01-17 22:20:00 -0300

    Update trab03-ed2.c

--- projetos/trab03-ed2.c
+++ projetos/trab03-ed2.c
@@ -1,257 +1,274 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #define ORDEM 5
-#define MAX_CHARS_NOME 30
-#define MAX_CHARS_CURSO 30
-#define RRN_NULL -1
-#define DELIMITER "#"
-#define DATA_FILENAME "turma.dat"
-#define INDEX_FILENAME "ibtree.idx"
-#define REG_SIZE 128
+#define MIN 2
 
-typedef struct {
-  char nome[MAX_CHARS_NOME];
-  char curso[MAX_CHARS_CURSO];
-  int RA_UNESP;
-} Aluno;
+// Tipo Aluno
+typedef struct Aluno{
+    char *nome;
+    char *curso;
+    int RA_UNESP;
+    int rrn;
+}Aluno;
 
-typedef struct {
-  int RA;
-  int RRN;
-} Chave;
+// Tipo Arvore
+typedef struct Arvore{
+    Aluno key[ORDEM+1];
+    struct Arvore *filho[ORDEM+1];
+    int numKeys;
+} Arvore;
 
-typedef struct {
-  int num_chaves;
-  Chave chaves[ORDEM-1];
-  int descendentes[ORDEM];
-  int folha;
-} Pagina;
+Arvore *raiz=NULL; //Raiz nula para inserir na folha depois
 
-// Função para ler um aluno do teclado
-Aluno ler_aluno() {
-  Aluno aluno;
-  printf("Nome: ");
-  scanf("%s", aluno.nome);
-  printf("Curso: ");
-  scanf("%s", aluno.curso);
-  printf("RA da UNESP: ");
-  scanf("%d", &aluno.RA_UNESP);
-  return aluno;
-}
+void declareArvore(Aluno aluno, FILE *arq);
+Arvore *newNo(Aluno keys, Arvore *filho);
+void insertNo(Aluno keys, int pos, Arvore *no,  Arvore *filho);
+void split(Aluno keys, Aluno *pointer, int pos,  Arvore *no,  Arvore *filho,  Arvore **new);
+int insertLeaf(Aluno keys, Aluno *pointer, Arvore *no, Arvore **filho);
+void verifyToInsert(Aluno keys);
+int search(Arvore *no, int val, FILE *arq);
+void indice(Arvore *treeB, FILE *arq);
+void writeArq(Aluno *aluno, FILE *arq);
 
-void inserir_aluno(Aluno aluno) {
-  // Abre o arquivo de dados em modo de append
-  FILE* data_file = fopen(DATA_FILENAME, "a");
-  // Calcula o RRN do novo aluno como o tamanho atual do arquivo de dados
-  int RRN = ftell(data_file);
-  // Escreve o aluno no final do arquivo
-  fwrite(&aluno, sizeof(Aluno), 1, data_file);
-  // Preenche o espaço restante do registro com o caractere '#'
-  int i;
-  for (i = 0; i < REG_SIZE - sizeof(Aluno); i++) {
-    fputc('#', data_file);
-  }
-  fclose(data_file);
-}
 
-int inserir_chave_pagina(int RA, int RRN, int RRN_pagina, FILE* index_file) {
-  // Lê a página do arquivo de índice
-  Pagina pagina;
-  fseek(index_file, RRN_pagina, SEEK_SET);
-  fread(&pagina, sizeof(Pagina), 1, index_file);
-  // Verifica se a página é uma folha
-  if (pagina.folha) {
-    // Insere a chave na página folha
-    int i = ORDEM - 2;
-    while (i >= 0 && RA < pagina.chaves[i].RA) {
-      pagina.chaves[i + 1] = pagina.chaves[i];
-      pagina.descendentes[i + 1] = pagina.descendentes[i];
-      i--;
+
+//Funcoes para arvore B
+void declareArvore(Aluno aluno, FILE *arq){
+    while (!feof(arq)){
+        fscanf(arq, "%d %d\n", &aluno.RA_UNESP, &aluno.rrn);
+        verifyToInsert(aluno);
     }
-    pagina.chaves[i + 1].RA = RA;
-    pagina.chaves[i + 1].RRN = RRN;
-    pagina.num_chaves++;
-    // Escreve a página atualizada no arquivo de índice
-    fseek(index_file, RRN_pagina, SEEK_SET);
-    fwrite(&pagina, sizeof(Pagina), 1, index_file);
-    // Retorna o RRN da página
-    return RRN_pagina;
-  } else {
-    // Encontra o descendente apropriado
-    int i = ORDEM - 2;
-    while (i >= 0 && RA < pagina.chaves[i].RA) {
-      i--;
+}
+// Criar um novo nó da árvore
+Arvore *newNo(Aluno keys, Arvore *filho){
+    Arvore *new;
+    new = (Arvore *)malloc(sizeof(Arvore));
+    new->key[1].RA_UNESP = keys.RA_UNESP;
+    new->key[1].rrn = keys.rrn;
+    new->numKeys = 1;
+    new->filho[0] = raiz;
+    new->filho[1] = filho;
+    return new;
+}
+
+// Inserir a nova chave em um nó da árvore
+void insertNo(Aluno keys, int pos, Arvore *no,  Arvore *filho) {
+    int j = no->numKeys;
+    while (j > pos) {
+      no->key[j + 1] = no->key[j];
+      no->filho[j + 1] = no->filho[j];
+      j--;
     }
-    i++;
-    // Insere a chave no descendente
-    int novo_RRN = inserir_chave_pagina(RA, RRN, pagina.descendentes[i], index_file);
-  }
+    no->key[j + 1].RA_UNESP = keys.RA_UNESP;
+    no->key[j + 1].rrn = keys.rrn;
+    no->filho[j + 1] = filho;
+    no->numKeys++;
 }
 
+// Função de split
+void split(Aluno keys, Aluno *pointer, int pos,  Arvore *no,  Arvore *filho,  Arvore **new) {
+    int meio, j;
 
+    if (pos > MIN)
+      meio = MIN + 1;
+    else
+      meio = MIN;
 
-void inserir_chave(int RA, int RRN) {
-  // Abre o arquivo de índice em modo de leitura e escrita
-  FILE* index_file = fopen(INDEX_FILENAME, "r+");
-  // Lê o RRN da página raiz do cabeçalho do arquivo
-  int raiz;
-  fread(&raiz, sizeof(int), 1, index_file);
-  // Insere a chave na árvore a partir da página raiz
-  int nova_raiz = inserir_chave_pagina(RA, RRN, raiz, index_file);
-  // Verifica se a página raiz foi dividida
-  if (nova_raiz != raiz) {
-    // Escreve a nova raiz no cabeçalho do arquivo de índice
-    fseek(index_file, 0, SEEK_SET);
-    fwrite(&nova_raiz, sizeof(int), 1, index_file);
-  }
-  fclose(index_file);
-}
-
-// Função recursiva para buscar uma chave em uma página da árvore-B
-Aluno buscar_chave(int RA, int rrn_pagina, FILE* index_file) {
-  // Lê a página do arquivo
-  Pagina pagina;
-  fseek(index_file, rrn_pagina, SEEK_SET);
-  fread(&pagina, sizeof(Pagina), 1, index_file);
-  // Procura a chave na página
-  int i;
-  for (i = 0; i < pagina.num_chaves; i++) {
-    if (pagina.chaves[i].RA == RA) {
-      // Se achou a chave, lê o aluno do arquivo de dados
-      Aluno aluno;
-      FILE* data_file = fopen(DATA_FILENAME, "r");
-      fseek(data_file, pagina.chaves[i].RRN, SEEK_SET);
-      fread(&aluno, sizeof(Aluno), 1, data_file);
-      fclose(data_file);
-      return aluno;
-    } else if (RA < pagina.chaves[i].RA) {
-      // Se a chave é menor, busca na página descendente à esquerda
-      break;
+    *new = (Arvore *)malloc(sizeof(Arvore));
+    j = meio + 1;
+    while (j <= ORDEM) {
+      (*new)->key[j - meio] = no->key[j];
+      (*new)->filho[j - meio] = no->filho[j];
+      j++;
     }
-  }
-  // Se a chave não foi encontrada e não há mais descendentes, retorna aluno vazio
-  if (pagina.descendentes[i] == RRN_NULL) {
-    Aluno aluno_vazio;
-    return aluno_vazio;
-  }
-  // Senão, busca recursivemente na página descendente
-  return buscar_chave(RA, pagina.descendentes[i], index_file);
+    no->numKeys = meio;
+    (*new)->numKeys = ORDEM - meio;
+
+    if (pos <= MIN)
+      insertNo(keys, pos, no, filho);
+
+    else
+      insertNo(keys, pos - meio, *new, filho);
+
+    pointer->RA_UNESP = no->key[no->numKeys].RA_UNESP;
+    pointer->rrn = no->key[no->numKeys].rrn;
+    (*new)->filho[0] = no->filho[no->numKeys];
+    no->numKeys--;
 }
 
-// Função para buscar um aluno pelo RA
-Aluno buscar_aluno(int RA) {
-  // Abre o arquivo de índice
-  FILE* index_file = fopen(INDEX_FILENAME, "r");
-  // Lê o RRN da página raiz do cabeçalho do arquivo
-  int rrn_raiz;
-  fread(&rrn_raiz, sizeof(int), 1, index_file);
-  // Se a árvore estiver vazia, retorna aluno vazio
-  if (rrn_raiz == RRN_NULL) {
-    Aluno aluno_vazio;
-    return aluno_vazio;
-  }
-  // Senão, busca a chave recursivamente na árvore
-  return buscar_chave(RA, rrn_raiz, index_file);
-}
-
-// Função para excluir um aluno pelo RA
-void excluir_aluno(int RA) {
-  // Abre o arquivo de índice
-  FILE* index_file = fopen(INDEX_FILENAME, "r+");
-  // Lê o RRN da página raiz do cabeçalho do arquivo
-  int rrn_raiz;
-  fread(&rrn_raiz, sizeof(int), 1, index_file);
-  // Se a árvore não estiver vazia, exclui a chave recursivamente
-  if (rrn_raiz != RRN_NULL) {
-    rrn_raiz = excluir_chave(RA, rrn_raiz, index_file);
-  }
-  // Atualiza o RRN da página raiz no cabeçalho do arquivo
-  fseek(index_file, 0, SEEK_SET);
-  fwrite(&rrn_raiz, sizeof(int), 1, index_file);
-  fclose(index_file);
-}
-
-// Função recursiva para excluir uma chave de uma página da árvore-B
-int excluir_chave(int RA, int rrn_pagina, FILE* index_file) {
-  // Lê a página do arquivo
-  Pagina pagina;
-  fseek(index_file, rrn_pagina, SEEK_SET);
-  fread(&pagina, sizeof(Pagina), 1, index_file);
-  // Procura a chave na página
-  int i;
-  for (i = 0; i < pagina.num_chaves; i++) {
-    if (pagina.chaves[i].RA == RA) {
-      // Se achou a chave, remove-a da página e retorna o RRN da página alterada
-      pagina.num_chaves--;
-      for (; i < pagina.num_chaves; i++) {
-        pagina.chaves[i] = pagina.chaves[i+1];
-      }
-      fseek(index_file, rrn_pagina, SEEK_SET);
-      fwrite(&pagina, sizeof(Pagina), 1, index_file);
-      return rrn_pagina;
-    } else if (RA < pagina.chaves[i].RA) {
-      // Se a chave é menor, exclui recursivamente da página descendente à esquerda
-      break;
+// Função para auxiliar na verificação para saber se é preciso criar um nó
+int insertLeaf(Aluno keys, Aluno *pointer, Arvore *no, Arvore **filho){
+    int pos;
+    // Verifica se nó não existe
+    if(!no){
+        *pointer = keys;
+        *filho = NULL; //Nulo para inserir na folha
+        return 1;
     }
-  }
-  // Se a chave não foi encontrada e não há mais descendentes, retorna RRN nulo
-  if (pagina.descendentes[i] == RRN_NULL) {
-    return RRN_NULL;
-  }
-  // Senão, exclui recursivamente da página descendente
-  int rrn_filho = excluir_chave(RA, pagina.descendentes[i], index_file);
-  // Se o filho não foi alterado, retorna o RRN da página atual
-  if (rrn_filho == pagina.descendentes[i]) {
-    return rrn_pagina;
-  }
-  // Senão, atualiza o RRN do filho na página atual e retorna o RRN alterado
-  pagina.descendentes[i] = rrn_filho;
-  fseek(index_file, rrn_pagina, SEEK_SET);
-  fwrite(&pagina, sizeof(Pagina), 1, index_file);
-  return rrn_pagina;
-}
-
-int main() {
-  int opcao;
-  do {
-    printf("\nMenu:\n");
-    printf("1. Inserir aluno\n");
-    printf("2. Buscar aluno\n");
-    printf("3. Excluir aluno\n");
-    printf("0. Sair\n");
-    printf("Opção: ");
-    scanf("%d", &opcao);
-    switch (opcao) {
-      case 1: {
-        Aluno aluno = ler_aluno();
-        inserir_aluno(aluno);
-        inserir_chave(aluno.RA_UNESP, ftell(DATA_FILENAME));
-        break;
-      }
-      case 2: {
-        int RA;
-        printf("RA da UNESP: ");
-        scanf("%d", &RA);
-        Aluno aluno = buscar_aluno(RA);
-        if (aluno.RA_UNESP == 0) {
-          printf("Aluno não encontrado\n");
-        } else {
-          printf("Nome: %s\n", aluno.nome);
-          printf("Curso: %s\n", aluno.curso);
-          printf("RA da UNESP: %d\n", aluno.RA_UNESP);
+    // Verificando se o RA passado é menor que o do nó da árvore
+    if(keys.RA_UNESP < no->key[1].RA_UNESP)
+        pos = 0;
+    else{
+        for (pos = no->numKeys; (keys.RA_UNESP < no->key[pos].RA_UNESP && pos > 1 ); pos--);{
+            if(keys.RA_UNESP == no->key[pos].RA_UNESP)
+                printf("\nRA JA INSERIDO!");
         }
-        break;
-      }
-      case 3: {
-        int RA;
-        printf("RA da UNESP: ");
-        scanf("%d", &RA);
-        excluir_aluno(RA);
-        break;
-      }
     }
-  } while (opcao != 0);
-  return 0;
+    // Verificando recursivamente na árvore
+    if(insertLeaf(keys, pointer, no->filho[pos], filho)){
+        if (no->numKeys < ORDEM)
+            insertNo(*pointer, pos, no, *filho);
+        else{
+            split(*pointer, pointer, pos, no, *filho, filho);
+            return 1; // Vai ser necessário criar um novo nó
+        }   
+    }
+    return 0;
+}
+
+// Função que verifica se será necessário criar um novo nó
+void verifyToInsert(Aluno keys){
+    Arvore *filho;
+    int retorno;
+    Aluno pointer;
+    
+    if(insertLeaf(keys, &pointer, raiz, &filho))
+        raiz = newNo(pointer, filho);
+}
+
+// Função para buscar um item na arvore
+int search(Arvore *no, int val, FILE *arq){
+    fseek(arq, 0, SEEK_SET);
+    Aluno aluno;
+    int i;
+
+    if(no){ 
+        for(i = 0; i < no->numKeys; i++){
+            if(search(no->filho[i], val, arq))
+                return 1;
+              
+            if(no->key[i + 1].RA_UNESP == val){ // Se o RA da proxima posição for igual ao buscado
+                char c[1];
+                
+                fseek(arq, no->key[i +1].rrn + 1, SEEK_SET); // Movendo ate a posição do RRN
+                fscanf(arq, "%d|", &aluno.RA_UNESP); // Lendo o RA do aluno
+                
+                aluno.nome = (char*)malloc(sizeof(70));
+                c[0] = fgetc(arq);
+
+                do{
+                    strcat(aluno.nome, c);
+                    c[0] = fgetc(arq);
+                }while(c[0] != '|'); // Lendo e formando a string "nome" ate o '|'
+
+                aluno.curso = (char*)malloc(sizeof(70));
+                c[0] = fgetc(arq);
+
+                do{
+                    strcat(aluno.curso, c);
+                    c[0] = fgetc(arq);
+                }while(c[0] != '|'); // Lendo e formando a string "curso" ate o '|'                
+                printf("Encontrado!\n%s|%s|%d\n", aluno.nome, aluno.curso, aluno.RA_UNESP);
+
+                return 1;
+            }        
+        }       
+        if(search(no->filho[i], val, arq))
+            return 1;
+    }
+
+    return 0;
+}
+
+//FUNCOES EXTRAS
+// Função que atualiza o índice da árvore
+void indice(Arvore *treeB, FILE *arq){
+    int i;
+    if(treeB){
+        for(i = 0; i < treeB->numKeys; i++){
+            indice(treeB->filho[i], arq);
+            fprintf(arq, "%d\n", treeB->key[i+1].RA_UNESP);
+            fprintf(arq, "%d\n", treeB->key[i+1].rrn);
+        }
+        indice(treeB->filho[i], arq);// chama recursivamente
+    }
+}
+
+// Função que escreve os dados dos alunos no arquivo e completa com '#'
+void writeArq(Aluno *aluno, FILE *arq){
+    int start, end;
+    fseek(arq, 0, SEEK_END);
+    start = ftell(arq);
+    fprintf(arq, "|%d|%s|%s|", aluno->RA_UNESP, aluno->nome, aluno->curso);
+    end = ftell(arq);
+
+    for(int i=0; i < 128 - (end-start); i++)
+        fprintf(arq, "#");
+    aluno->rrn = start;
+}
+
+// Função principal
+int main(){
+
+    int key, ch;
+    int op=0;
+    Aluno aluno;
+    Arvore arvore;
+    FILE *data;
+    FILE *index;
+    char nome[60];
+    char curso[60];
+
+    data = fopen("turma.dat", "a+");
+    index = fopen("ibtree.idx", "a+");
+    
+    do
+    {
+        printf("\nDigite a opcao que deseja: ");
+        printf("\n 1 - Inserir aluno");
+        printf("\n 2 - Buscar aluno");
+        printf("\n 3 - Sair\n");
+        scanf("%d", &op);
+
+        switch (op)
+        {
+        case 1:
+            printf("\nNome do aluno a inserir: ");
+            scanf(" %[^\n]c", nome);
+            aluno.nome = (char *)malloc(sizeof(strlen(nome)+1));
+            strcpy(aluno.nome, nome);
+
+            printf("\nCurso do aluno a inserir: ");
+            scanf(" %[^\n]c", curso);
+            aluno.curso = (char *)malloc(sizeof(strlen(curso)+1));
+            strcpy(aluno.curso, curso);
+            int tamanho;
+            
+            do{
+                printf("\nDigite o RA: ");
+                scanf("%d", &aluno.RA_UNESP);
+               
+            } while (aluno.RA_UNESP > 100000000 && aluno.RA_UNESP < 99999999);
+
+            writeArq(&aluno, data);
+            verifyToInsert(aluno);
+            fseek(index, 0, SEEK_END);
+            indice(raiz, index);
+            break;
+        case 2:
+            printf("\nDigite o RA a ser buscado: ");
+            scanf("%d", &key);
+            search(raiz, key, data);
+            break;
+        
+        default:
+            break;
+        }
+    } while (op!=3);
+
+    fclose(index);
+    fclose(data);
+    return 0;
 }
